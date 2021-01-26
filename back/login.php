9142c2290b1c93ed10e5c203b469e0bdf98e8ea3
eveuuh <eve.jourdan@gmail.com>
2021-01-26 11:07:28 +0100

    update sql

--- back/login.php
+++ back/login.php
@@ -5,7 +5,7 @@ include ("header.php");
 $user = $_POST['pseudo']; // Comme le formulaire est soumis avec la méthode get, on utilise le tableau $_GET pour récupérer les valeurs des champs du formulaire
 $pass = $_POST['password']; // On a le mot de passe saisi par l'utilisateur
 
-$req = "SELECT pseudo,password,photo,id FROM Adherents left join Profils on Adherents.id=AdherentID WHERE pseudo = :pseudo ";    
+$req = "SELECT pseudo,password,photo,Adherents.id FROM Adherents left join Profils on Adherents.id=AdherentID WHERE pseudo = :pseudo ";    
 
 try {
     $statement = $connexion->prepare($req);
